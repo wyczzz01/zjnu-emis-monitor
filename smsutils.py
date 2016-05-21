f356793d121efa555051b70a9e13972c9c89d166
ShangJJ <709218051@qq.com>
2016-05-21 13:09:14 +0800

    Add BmobSms

--- smsutils.py
+++ smsutils.py
@@ -1,41 +1,87 @@
+from abc import abstractmethod, ABCMeta
 import json
 import requests
 import grequests
 from datetime import datetime
 
-URL_CC_TOKEN = 'https://oauth.api.189.cn/emp/oauth2/v3/access_token'
-URL_SEND_MESSAGE = 'http://api.189.cn/v2/emp/templateSms/sendSms'
-APP_ID = '726195780000251941'
-APP_SECRET = 'e1fc0f5cfba335ea8b8d5f8d698cf218'
+
+class BaseSmsUtils:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.get_credentials()
+
+    @abstractmethod
+    def send(self, receivers, content): pass
+
+    @abstractmethod
+    def get_credentials(self): pass
 
 
-class SmsUtils:
+class Open189SmsUtils(BaseSmsUtils):
 
-    def __init__(self, app_id=APP_ID, app_secret=APP_SECRET):
-        self.access_token = self.get_access_token(app_id, app_secret)
+    def __init__(self):
+        super().__init__()
+        self.url_cc_token = 'https://oauth.api.189.cn/emp/oauth2/v3/access_token'
+        self.url_send_message = 'http://api.189.cn/v2/emp/templateSms/sendSms'
+        self.app_id = '726195780000251941'
+        self.app_secret = 'e1fc0f5cfba335ea8b8d5f8d698cf218'
 
-    def get_access_token(self, app_id, app_secret):
+    def get_credentials(self):
         data = {
             'grant_type': 'client_credentials',
-            'app_id': app_id,
-            'app_secret': app_secret,
+            'app_id': self.app_id,
+            'app_secret': self.app_secret,
         }
-        res = requests.post(URL_CC_TOKEN, data=data)
+        res = requests.post(self.url_cc_token, data=data)
         if res.content is not None:
             content = json.loads(res.content.decode())
-            return content["access_token"]
+            self.access_token = content["access_token"]
 
     def send(self, receivers, content=''):
         rs = set()
         for each in receivers:
             data = {
-                'app_id': APP_ID,
+                'app_id': self.app_id,
                 'access_token': self.access_token,
                 'acceptor_tel': each,
                 'template_id': '91550991',
                 'template_param': '{}',
                 'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             }
-            rs.add(grequests.post(URL_SEND_MESSAGE, data=data))
+            rs.add(grequests.post(self.url_send_message, data=data))
         grequests.map(rs)
 
+
+class BmobSmsUtils(BaseSmsUtils):
+
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://api.bmob.cn/1/requestSmsCode'
+        self.bmob_app_id = '2b3840dbaea06dca9e822699a20b22be'
+        self.bmob_rest_api_key = '31165eb3c129fffd72c021a7c2f9c2f6'
+
+    def send_sms(self, receivers, content):
+        pass
+
+    def send_sms_template(self, receivers, template_name):
+        rs = set()
+        headers = {
+            'X-Bmob-Application-Id': self.bmob_app_id,
+            'X-Bmob-REST-API-Key': self.bmob_rest_api_key,
+            'Content-Type': 'application/json',
+        }
+        for each in receivers:
+            data = {
+                'mobilePhoneNumber': each,
+                'template': template_name,
+            }
+            rs.add(grequests.post(data=data, headers=headers))
+        grequests.map(rs)
+
+    def get_credentials(self):
+        pass
+
+
+if __name__ == '__main__':
+    BmobSmsUtils().send_sms_template(['18395960722'], 'new_score')
